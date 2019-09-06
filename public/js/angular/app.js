6922a4fa277cb0fe62ed19af8cf1efbc79c7e008
shubham-BE <sbhatt@bhaskerenterprises.in>
2019-09-06 16:10:42 +0530

    initail commit

---
+++ public/js/angular/app.js
@@ -0,0 +1,45 @@
+var app = angular.module('LaravelApp',
+    [
+        'ngRoute',
+        'ui.calendar'
+    ], function ($interpolateProvider, $locationProvider ,$routeProvider)
+    {
+        $interpolateProvider.startSymbol('<%');
+        $interpolateProvider.endSymbol('%>');
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+    });
+
+//Used to get the main absolute path.
+var main_path = location.pathname;
+
+if (main_path.indexOf("/public") == -1)
+{
+    var url = '';
+}
+else
+{
+    var url = main_path.substr(0, main_path.indexOf("/public") + 8);
+}
+
+app.filter('rawHtml', [
+    '$sce',
+    function ($sce)
+    {
+        return function (val)
+        {
+            return $sce.trustAsHtml(val);
+        };
+    }
+]);
+
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
